a231573eb35a7e9767a8533b27ba63b8d5882e48
Simon Duck <simon.duck@softwire.com>
2025-04-22 15:01:32 +0100

    Fizz on the 3!

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,7 +1,13 @@
 
 function fizzBuzz() {
-  for (i =0; i < 100; i++) {
-    console.log(i+1);
+
+
+  for (i =1; i < 101; i++) {
+     if (i % 3 === 0) {
+      console.log("Fizz");
+    } else {
+      console.log(i);
+    }
 }}
 
 fizzBuzz();
